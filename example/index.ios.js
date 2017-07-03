1e5698bb31401bb24c97c8c7e208e1e9a4bf6146
dbuarque <dio@getty.io>
2017-07-03 13:36:32 -0300

    release

--- example/index.ios.js
+++ example/index.ios.js
@@ -4,28 +4,15 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
+import RealpubNativeChat from "@realpub/native-chat";
 export default class ReactNativeChatDemo extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <RealpubNativeChat />
       </View>
     );
   }
@@ -34,20 +21,10 @@ export default class ReactNativeChatDemo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
 });
 
-AppRegistry.registerComponent('ReactNativeChatDemo', () => ReactNativeChatDemo);
+AppRegistry.registerComponent("ReactNativeChatDemo", () => ReactNativeChatDemo);
